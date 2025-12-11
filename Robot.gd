a9c92675e6645a0491624ae81859f250c408921e
Stefan Schulz <stefan.schulz.1976@googlemail.com>
2025-12-11 14:34:26 +0100

    further work

--- Robot.gd
+++ Robot.gd
@@ -4,6 +4,8 @@ extends CharacterBody3D
 const SPEED = 25.0
 const JUMP_VELOCITY = 4.5
 
+@onready var animationTree: AnimationTree = $AnimationTree
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -15,16 +17,23 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_down", "ui_up", "ui_left", "ui_right")
+	var input_dir := Input.get_vector("walkBackward", "walkForward", "ui_left", "ui_right")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
+		walk(delta)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+		idle()
 
 	move_and_slide()
 
-func walk(delta: float) -> void:
-	pass
+func walk(_delta: float) -> void:
+	animationTree["parameters/conditions/idle"] = false
+	animationTree["parameters/conditions/walk"] = true	
+	
+func idle() -> void:
+	animationTree["parameters/conditions/idle"] = true
+	animationTree["parameters/conditions/walk"] = false	

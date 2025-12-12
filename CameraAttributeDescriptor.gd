3caffc7dd8b2d3e856ef685d6f6ff4c02a9d675e
Stefan Schulz <stefan.schulz.1976@googlemail.com>
2025-12-12 13:34:45 +0100

    more camera pivot tweening

---
+++ CameraAttributeDescriptor.gd
@@ -0,0 +1,9 @@
+class_name CameraAttributeDescriptor
+extends Object
+
+var position: Vector3
+var rotation: Vector3
+
+func _init(aPosition: Vector3, aRotation: Vector3) -> void:
+	position = aPosition
+	rotation = aRotation
--- Robot.gd
+++ Robot.gd
@@ -1,9 +1,10 @@
 class_name Robot
 extends CharacterBody3D
 
-const SPEED = 25.0
+const SPEED = 5.0
+const CAMERA_TWEEN_SPEED = 0.75
 
-enum WalkAction {FORWARD, BACKWARD, NONE}
+enum MoveAction {WALK_FORWARD, WALK_BACKWARD, NONE}
 
 @onready var animationTree: AnimationTree = $AnimationTree
 @onready var camera: Camera3D = $Camera3D
@@ -12,40 +13,27 @@ enum WalkAction {FORWARD, BACKWARD, NONE}
 @onready var backposCameraPivot: Node3D = $TestRiggedRobot/Armature/Skeleton3D/spine/Body/BackPosCameraPivot
 @onready var frontposCameraPivot: Node3D = $TestRiggedRobot/Armature/Skeleton3D/spine/Body/FrontPosCameraPivot
 
-var walking: bool = false
-var actualWalkAction: WalkAction
+var actualMoveAction: MoveAction
 
-func _ready() -> void:
-	print(str("shoulderCameraPivot.rotation.y --> ", str(rad_to_deg(shoulderCameraPivot.rotation.y))))
-	print(str("backposCameraPivot.rotation.y --> ", str(rad_to_deg(backposCameraPivot.rotation.y))))
-	print(str("frontposCameraPivot.rotation.y --> ", str(rad_to_deg(frontposCameraPivot.rotation.y))))
-	
 func _process(_delta: float) -> void:
 	tweenCamera(_delta)
 	
 func tweenCamera(_delta: float) -> void:
+	var attributes: CameraAttributeDescriptor = getCameraAttributes(actualMoveAction)	
+	var cameraPositionTween = get_tree().create_tween()
+	cameraPositionTween.tween_property(camera, "global_position", attributes.position, CAMERA_TWEEN_SPEED)	
+	var cameraRotationTween = get_tree().create_tween()
+	cameraRotationTween.tween_property(camera, "global_rotation_degrees", attributes.rotation, CAMERA_TWEEN_SPEED)	
 	
-	var desiredCamPos = Vector3.ZERO
-	var desiredCamRotation: float = 0.0
-	
-	match actualWalkAction:
-		WalkAction.FORWARD:
-			print(str(WalkAction.FORWARD))
-		WalkAction.BACKWARD:
-			print(str(WalkAction.BACKWARD))
-		WalkAction.NONE:
-			print(str(WalkAction.NONE))
-	
-	if walking:
-		desiredCamPos = backposCameraPivot.global_position
-		desiredCamRotation = -90.0
-	else:
-		desiredCamPos = shoulderCameraPivot.global_position
-		desiredCamRotation = -90.0
-		
-	var cameraTween = get_tree().create_tween()
-	cameraTween.tween_property(camera, "global_position", desiredCamPos, 1.0)
-	cameraTween.tween_property(camera, "rotation_degrees", desiredCamRotation, 1.0)
+func getCameraAttributes(moveAction: MoveAction) -> CameraAttributeDescriptor:
+	match moveAction:
+		MoveAction.WALK_FORWARD:
+			return CameraAttributeDescriptor.new(backposCameraPivot.global_position, Vector3(0.0, -90.0, 0.0))
+		MoveAction.WALK_BACKWARD:
+			return CameraAttributeDescriptor.new(frontposCameraPivot.global_position, Vector3(0.0,90.0, 0.0))
+		MoveAction.NONE:
+			return CameraAttributeDescriptor.new(shoulderCameraPivot.global_position, Vector3(0.0, -90.0, 0.0))
+	return null			
 		
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
@@ -61,19 +49,17 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 		idle()
-		walking = false
 	move_and_slide()
 	
 func trackWalkAction() -> void:
 	if Input.is_action_pressed("walkForward"):
-		actualWalkAction = WalkAction.FORWARD
+		actualMoveAction = MoveAction.WALK_FORWARD
 	elif Input.is_action_pressed("walkBackward"):
-		actualWalkAction = WalkAction.BACKWARD
+		actualMoveAction = MoveAction.WALK_BACKWARD
 	else:
-		actualWalkAction = WalkAction.NONE
+		actualMoveAction = MoveAction.NONE
 
 func walk(_delta: float) -> void:
-	walking = true
 	animationTree["parameters/conditions/idle"] = false
 	animationTree["parameters/conditions/walk"] = true	
 	

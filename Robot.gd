3ff652e8c7b4903cb0c5ecf8abe8ec84f4533168
Stefan Schulz <stefan.schulz.1976@googlemail.com>
2025-12-12 14:42:28 +0100

    integrated turns

--- Robot.gd
+++ Robot.gd
@@ -4,16 +4,35 @@ extends CharacterBody3D
 const SPEED = 5.0
 const CAMERA_TWEEN_SPEED = 0.75
 
-enum MoveAction {WALK_FORWARD, WALK_BACKWARD, NONE}
+enum MoveAction {WALK_FORWARD, WALK_BACKWARD, NONE, TURN_LEFT, TURN_RIGHT}
 
 @onready var animationTree: AnimationTree = $AnimationTree
 @onready var camera: Camera3D = $Camera3D
 
-@onready var shoulderCameraPivot: Node3D = $TestRiggedRobot/Armature/Skeleton3D/spine/Body/ShoulderCameraPivot
-@onready var backposCameraPivot: Node3D = $TestRiggedRobot/Armature/Skeleton3D/spine/Body/BackPosCameraPivot
-@onready var frontposCameraPivot: Node3D = $TestRiggedRobot/Armature/Skeleton3D/spine/Body/FrontPosCameraPivot
+@onready var rightShoulderCameraPivot: Node3D = $TestRiggedRobot/Armature/Skeleton3D/spine/Body/RightShoulderCameraPivot
+@onready var leftShoulderCameraPivot: Node3D = $TestRiggedRobot/Armature/Skeleton3D/spine/Body/LeftShoulderCameraPivot
+@onready var backPosCameraPivot: Node3D = $TestRiggedRobot/Armature/Skeleton3D/spine/Body/BackPosCameraPivot
+@onready var frontPosCameraPivot: Node3D = $TestRiggedRobot/Armature/Skeleton3D/spine/Body/FrontPosCameraPivot
+
+@onready var topPosCameraPivot: Node3D = $TestRiggedRobot/Armature/Skeleton3D/spine/Body/TopPosCameraPivot
+
+var mouse_motion := Vector2.ZERO
 
 var actualMoveAction: MoveAction
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+			mouse_motion = -event.relative * 0.001		
+			if event.relative.x >= 0.0:
+				actualMoveAction = MoveAction.TURN_RIGHT
+			else:
+				actualMoveAction = MoveAction.TURN_LEFT
+	if event.is_action_pressed("ui_cancel"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
 func _process(_delta: float) -> void:
 	tweenCamera(_delta)
@@ -28,17 +47,27 @@ func tweenCamera(_delta: float) -> void:
 func getCameraAttributes(moveAction: MoveAction) -> CameraAttributeDescriptor:
 	match moveAction:
 		MoveAction.WALK_FORWARD:
-			return CameraAttributeDescriptor.new(backposCameraPivot.global_position, Vector3(0.0, -90.0, 0.0))
+			print("MoveAction.WALK_FORWARD")
+			return CameraAttributeDescriptor.new(backPosCameraPivot.global_position, Vector3(0.0, rotation_degrees.y-90.0, 0.0))
 		MoveAction.WALK_BACKWARD:
-			return CameraAttributeDescriptor.new(frontposCameraPivot.global_position, Vector3(0.0,90.0, 0.0))
+			print("MoveAction.WALK_BACKWARD")
+			return CameraAttributeDescriptor.new(frontPosCameraPivot.global_position, Vector3(0.0, rotation_degrees.y+90.0, 0.0))
 		MoveAction.NONE:
-			return CameraAttributeDescriptor.new(shoulderCameraPivot.global_position, Vector3(0.0, -90.0, 0.0))
-	return null			
+			print("MoveAction.NONE")
+			return CameraAttributeDescriptor.new(rightShoulderCameraPivot.global_position, Vector3(0.0, rotation_degrees.y-90.0, 0.0))
+		MoveAction.TURN_RIGHT:
+			print("MoveAction.TURN_RIGHT")
+			return null
+		MoveAction.TURN_LEFT:
+			print("MoveAction.TURN_LEFT")
+			return null
+	return null
 		
 func _physics_process(delta: float) -> void:
+	handle_rotation(mouse_motion)
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	trackWalkAction()
+	trackWalkActionFromInput()
 	var input_dir := Input.get_vector("walkBackward", "walkForward", "ui_left", "ui_right")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
@@ -51,13 +80,16 @@ func _physics_process(delta: float) -> void:
 		idle()
 	move_and_slide()
 	
-func trackWalkAction() -> void:
+func handle_rotation(motion: Vector2) -> void:
+	rotation.y += motion.x	
+	
+func trackWalkActionFromInput() -> void:
 	if Input.is_action_pressed("walkForward"):
 		actualMoveAction = MoveAction.WALK_FORWARD
 	elif Input.is_action_pressed("walkBackward"):
 		actualMoveAction = MoveAction.WALK_BACKWARD
 	else:
-		actualMoveAction = MoveAction.NONE
+		actualMoveAction = MoveAction.NONE		
 
 func walk(_delta: float) -> void:
 	animationTree["parameters/conditions/idle"] = false
